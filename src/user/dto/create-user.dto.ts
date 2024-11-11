8ed107a4a6629d540a73e44bb48f8fab68e0932d
Julse77 <wrs0707@gmail.com>
2024-11-11 20:43:12 +0900

    Feat: create-user-dto 업데이트
    username, email, password, phone에 관련한 데코레이터를 업데이트 함

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,6 @@
 import {
   IsEmail,
+  IsPhoneNumber,
   IsString,
   Matches,
   MaxLength,
@@ -10,16 +11,21 @@ export class CreateUserDto {
   @IsString()
   @MinLength(4)
   @MaxLength(20)
-  username: string;
+  readonly username: string;
 
   @IsEmail()
-  email: string;
+  readonly email: string;
 
-  @MinLength(4)
+  @MinLength(10)
   @MaxLength(20)
-  @Matches(/^[a-zA-Z0-9]*$/, {
-    // 영어, 숫자만 가능한 유효성 체크
-    message: `영어, 숫자, !, @, #, - 등 기호를 추가해주세요.`,
-  })
-  password: string;
+  @Matches(
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,}$/,
+    {
+      message: `하나 이상의 알파벳 문자, 숫자 그리고 $, @, $, !, %, *, #, ?, & 등 기호를 추가하여 최소 10자 이상, 20자 이하의 비밀번호를 생성하세요.`,
+    },
+  )
+  readonly password: string;
+
+  @IsPhoneNumber('KR', { message: `한국 폰 번호를 입력해주세요.` })
+  readonly phone: string;
 }
