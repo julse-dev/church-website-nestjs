d74864b27106b9a4e399d53c147273401f94337c
julse-dev <julse.dev@gmail.com>
2025-05-23 16:09:23 +0900

    Feat: 회원정보 기능 구현(회원가입, JWT를 사용한 로그인, 로그아웃)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,13 +7,15 @@ import { CredentialAuthDto } from './dto/credential-auth.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  token: string = 'access_token';
+
   @Post('signin')
   async signIn(
     @Body(ValidationPipe) credentialAuthDto: CredentialAuthDto,
     @Res() res: Response,
   ) {
     const accessToken = await this.authService.signIn(credentialAuthDto);
-    res.cookie('access_token', accessToken, {
+    res.cookie(this.token, accessToken, {
       httpOnly: true,
       secure: true,
       sameSite: 'strict',
@@ -22,5 +24,18 @@ export class AuthController {
     return res.send({
       message: 'Login successful',
     });
+  }
+
+  @Post('signout')
+  async signOut(@Res() res: Response) {
+    res.clearCookie(this.token, {
+      httpOnly: true,
+      secure: true, // https 사용 시 true
+      sameSite: 'strict',
+    });
+
+    console.log('logout');
+
+    return res.status(200).json({ message: '로그아웃 되었습니다.' });
   }
 }
