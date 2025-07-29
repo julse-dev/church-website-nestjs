eefe8518a57c9d260b3f3b549e78136a6df5e722
julse-dev <julse.dev@gmail.com>
2025-07-29 11:13:59 +0900

    chore: dependencies 최신 업데이트
    - Upgrade @nestjs/common, @nestjs/core, @nestjs/platform-express, and @nestjs/typeorm to version 11.x
    - Update bcrypt and @types/bcrypt to latest compatible versions
    - Upgrade class-validator, pg, reflect-metadata, and rxjs to latest versions
    - Update devDependencies: @nestjs/cli, @nestjs/schematics, @nestjs/testing, @types/express, @types/jest, @types/node, @types/supertest, @typescript-eslint/eslint-plugin, @typescript-eslint/parser, eslint, eslint-config-prettier, eslint-plugin-prettier, jest, prettier, supertest, ts-jest, ts-loader, ts-node, and typescript to latest versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,21 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'eslint:recommended',
   ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  env: { node: true, jest: true },
+  ignorePatterns: ['.eslintrc.js', 'src/**/*.test.ts', 'src/**/*.d.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/strict-boolean-expressions': [
+      2,
+      { allowString: false, allowNumber: false },
+    ],
   },
 };
